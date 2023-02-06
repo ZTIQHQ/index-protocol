7d44cf2d5042cba1306a06eeffe320d1cd845132
SnakePoison <Ramysmelo@gmail.com>
2023-02-06 18:17:50 -0500

    style: update feature spdx identifiers.
    Later compilers require valid spx identifiers.

--- contracts/interfaces/external/IArrakisVaultV1.sol
+++ contracts/interfaces/external/IArrakisVaultV1.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: GPL-3.0
-
 /*
     Copyright 2022 Set Labs Inc.
     Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +9,7 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-    SPDX-License-Identifier: Apache License, Version 2.0
+    SPDX-License-Identifier: Apache-2.0
 */
 
 pragma solidity 0.6.10;
