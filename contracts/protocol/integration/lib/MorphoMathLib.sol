6a3a7ec4fe12ea2466cad6df3d6ba41d28e6cc41
ckoopmann <c.k.e.koopmann@gmail.com>
2024-08-15 17:32:12 +0800

    Use SafeMath library in MorphoMathLib

--- contracts/protocol/integration/lib/MorphoMathLib.sol
+++ contracts/protocol/integration/lib/MorphoMathLib.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.6.10;
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 /// @title MathLib
 /// @author Morpho Labs
 /// @notice Library to manage fixed-point arithmetic.
 library MorphoMathLib {
+    using SafeMath for uint256;
+
     /// @dev Returns (`x` * `y`) / `d` rounded down.
     function mulDivDown(uint256 x, uint256 y, uint256 d) internal pure returns (uint256) {
-        return (x * y) / d;
+        return (x.mul(y)).div(d);
     }
 
     /// @dev Returns (`x` * `y`) / `d` rounded up.
     function mulDivUp(uint256 x, uint256 y, uint256 d) internal pure returns (uint256) {
-        return (x * y + (d - 1)) / d;
+        return x.mul(y).add(d.sub(1)).div(d);
     }
 }
 
