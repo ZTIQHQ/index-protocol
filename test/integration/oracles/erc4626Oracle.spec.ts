6677edc0c858f8084a6b14e71fb20efc6d96e32f
Pranav Bhardwaj <pblivin0x@gmail.com>
2024-09-24 20:48:52 -0400

    add tests

--- test/integration/oracles/erc4626Oracle.spec.ts
+++ test/integration/oracles/erc4626Oracle.spec.ts
@@ -5,7 +5,7 @@ import { Address } from "@utils/types";
 import { Account } from "@utils/test/types";
 import { ERC4626ConverterMock, ERC4626Oracle } from "@utils/contracts";
 import DeployHelper from "@utils/deploys";
-import { ether, usdc } from "@utils/index";
+import { ether } from "@utils/index";
 import {
   getAccounts,
   getWaffleExpect,
@@ -21,10 +21,12 @@ describe("ERC4626Oracle", () => {
   let deployer: DeployHelper;
   let setup: SystemFixture;
 
-  let usdcVault: ERC4626ConverterMock;
+  let usdcLowDecimalVault: ERC4626ConverterMock;
+  let usdcHighDecimalVault: ERC4626ConverterMock;
   let daiVault: ERC4626ConverterMock;
 
-  let erc4626UsdcOracle: ERC4626Oracle;
+  let erc4626UsdcLowDecimalOracle: ERC4626Oracle;
+  let erc4626UsdcHighDecimalOracle: ERC4626Oracle;
   let erc4626DaiOracle: ERC4626Oracle;
 
   let price: BigNumber;
@@ -41,12 +43,17 @@ describe("ERC4626Oracle", () => {
 
     price = ether(1.02);
 
-    usdcVault = await deployer.mocks.deployERC4626ConverterMock(setup.usdc.address, 18, price);
+    usdcLowDecimalVault = await deployer.mocks.deployERC4626ConverterMock(setup.usdc.address, 6, price);
+    usdcHighDecimalVault = await deployer.mocks.deployERC4626ConverterMock(setup.usdc.address, 18, price);
     daiVault = await deployer.mocks.deployERC4626ConverterMock(setup.dai.address, 18, price);
 
-    erc4626UsdcOracle = await deployer.oracles.deployERC4626Oracle(
-      usdcVault.address,
-      "usdcVault-usdc Oracle"
+    erc4626UsdcLowDecimalOracle = await deployer.oracles.deployERC4626Oracle(
+      usdcLowDecimalVault.address,
+      "usdcLowDecimalVault-usdc Oracle"
+    );
+    erc4626UsdcHighDecimalOracle = await deployer.oracles.deployERC4626Oracle(
+      usdcHighDecimalVault.address,
+      "usdcHighDecimalVault-usdc Oracle"
     );
     erc4626DaiOracle = await deployer.oracles.deployERC4626Oracle(
       daiVault.address,
@@ -61,8 +68,8 @@ describe("ERC4626Oracle", () => {
     let subjectDataDescription: string;
 
     before(async () => {
-      subjectVaultAddress = usdcVault.address;
-      subjectDataDescription = "usdcVault-usdc Oracle";
+      subjectVaultAddress = daiVault.address;
+      subjectDataDescription = "daiVault-dai Oracle";
     });
 
     async function subject(): Promise<ERC4626Oracle> {
@@ -73,23 +80,23 @@ describe("ERC4626Oracle", () => {
     }
 
     it("sets the correct vault address", async () => {
-      const erc4626UsdcOracle = await subject();
-      const vaultAddress = await erc4626UsdcOracle.vault();
+      const oracle = await subject();
+      const vaultAddress = await oracle.vault();
       expect(vaultAddress).to.equal(subjectVaultAddress);
     });
 
 
     it("sets the correct full units", async () => {
-      const erc4626UsdcOracle = await subject();
-      const underlyingFullUnit = await erc4626UsdcOracle.underlyingFullUnit();
-      const vaultFullUnit = await erc4626UsdcOracle.vaultFullUnit();
-      expect(underlyingFullUnit).to.eq(usdc(1));
+      const oracle = await subject();
+      const underlyingFullUnit = await oracle.underlyingFullUnit();
+      const vaultFullUnit = await oracle.vaultFullUnit();
+      expect(underlyingFullUnit).to.eq(ether(1));
       expect(vaultFullUnit).to.eq(ether(1));
     });
 
     it("sets the correct data description", async () => {
-      const erc4626UsdcOracle = await subject();
-      const actualDataDescription = await erc4626UsdcOracle.dataDescription();
+      const oracle = await subject();
+      const actualDataDescription = await oracle.dataDescription();
       expect(actualDataDescription).to.eq(subjectDataDescription);
     });
   });
@@ -113,7 +120,18 @@ describe("ERC4626Oracle", () => {
 
     describe("when the vault has higher decimals than the underlying", async () => {
       beforeEach(async () => {
-        subjectOracle = erc4626UsdcOracle;
+        subjectOracle = erc4626UsdcHighDecimalOracle;
+      });
+
+      it("returns the correct value", async () => {
+        const result = await subject();
+        expect(result).to.eq(price);
+      });
+    });
+
+    describe("when the vault and the underlying have decimals below 18", async () => {
+      beforeEach(async () => {
+        subjectOracle = erc4626UsdcLowDecimalOracle;
       });
 
       it("returns the correct value", async () => {
